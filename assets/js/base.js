248eb4a517aa0abaf47e753d181dc89fbdbfe4c4
Job <bojlk2@hotmail.com>
2021-11-23 22:28:11 -0600

    add exercise 1

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,5 +1,21 @@
-let operation = prompt('Introduce')
-let operation = prompt()
-let number1 = 0
-let number2 = 0
+const number1 = parseInt(prompt('Introduce un numero'))
+const number2 = parseInt(prompt('Introduce otro numero'))
 
+if(isNaN(number1) === false && isNaN(number2) === false){
+    if(number1 > number2){
+        console.log('La división es =', number1/number2)
+    } else if(number1 < number2){
+        console.log('La suma es =', number1+number2)
+    } else if(number1 === number2){
+        console.log('La multiplicación es =', number1*number2)
+    }
+} else {
+    console.error('Algunos de los números no son tipo number')
+}
+
+for(i=1; i <= 10; i++){
+    console.log(`Tabla del ${i}`)
+    for(j = 1; j <= 10; j++){
+        console.log(`${i} x ${j} = ${i*j}`)
+    }
+}
