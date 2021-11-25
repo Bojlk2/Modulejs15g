a114e2a2e2cb9adfd64f369f309e3349a0b8322f
Job <bojlk2@hotmail.com>
2021-11-24 22:30:28 -0600

    Class2

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,21 +1,43 @@
-const number1 = parseInt(prompt('Introduce un numero'))
-const number2 = parseInt(prompt('Introduce otro numero'))
+// let celsius = parseInt(prompt('Introduce los grados Celsius de tu localidad'))
+// let far = ((celsius * 9/5) + 32)
+// if(!isNaN(celsius)){
+//     alert(`La temperatura actual en grados Fahrenheit es: ${far}`)
+// } else {
+//     alert('Debe ser un número')
+// }
 
-if(isNaN(number1) === false && isNaN(number2) === false){
-    if(number1 > number2){
-        console.log('La división es =', number1/number2)
-    } else if(number1 < number2){
-        console.log('La suma es =', number1+number2)
-    } else if(number1 === number2){
-        console.log('La multiplicación es =', number1*number2)
-    }
-} else {
-    console.error('Algunos de los números no son tipo number')
-}
 
-for(i=1; i <= 10; i++){
-    console.log(`Tabla del ${i}`)
-    for(j = 1; j <= 10; j++){
-        console.log(`${i} x ${j} = ${i*j}`)
-    }
-}
+// let n1 = parseInt(prompt('Introduce el primer número'))
+// let n2 = parseInt(prompt('Introduce el segundo número'))
+// let n3 = parseInt(prompt('Introduce el tercer número'))
+
+// if (!isNaN(n1) && !isNaN(n2) && !isNaN(n3)){
+// ((n1 > n2) && (n1 > n3)) ? console.log(`${n1} es el mayor de los números`) :
+// ((n2 > n1) && (n2 > n3)) ? console.log(`${n2} es el mayor de los números`) :
+// ((n3 > n1) && (n3 > n2))
+// console.log(`${n3} es el mayor de los números`)
+// } else {console.log('Introducir solo números')
+// }
+const a = 'Soleado'
+const b = 'Lluvioso'
+const c = 'Nevando'
+const d = 'Nublado'
+let weather = prompt('El clima de tu ciudad es: a. Soleado, b. Lluvioso, c. Nevando, d. Nublado')
+let cel = parseInt(prompt('Introduce la temperatura de tu ciudad en °C'))
+let farhen = ((cel * 9/5) + 32)
+
+switch(a || b || c || d){
+    case a:
+        console.log(`En tu ciudad, el día es ${weather} con una temperatura de ${farhen} °F`)
+        break
+    case b:
+        console.log(`En tu ciudad, el día es ${weather} con una temperatura de ${farhen} °F`)
+        break
+    case c:
+        console.log(`En tu ciudad, el día es ${weather} con una temperatura de ${farhen} °F`)
+        break
+    case d:
+        console.log(`En tu ciudad, el día es ${weather} con una temperatura de ${farhen} °F`)
+        break
+} 
+    
