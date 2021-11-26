89ecbe4a53b7cf2e0f5d38aece2e27c8c0306424
Job <bojlk2@hotmail.com>
2021-11-25 19:23:49 -0600

    Dayweek with tolowercase

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -39,3 +39,42 @@ let n1 = parseInt(prompt('Introduce un número'))
 let n2 = parseInt(prompt('Introduce otro número'))
 
 let show = (n1 > n2) ? console.log(`El número mayor es ${n1} y el menor es ${n2}`) : console.log(`El número mayor es ${n2} y el menor es ${n1}`)
+
+/**
+ * 
+ * Pedir al usuario el dia de la semana 
+ * p.ej. lunes, martes, ...
+ * Imprimir en consola el número del dia de la semana equivalente
+ * es decir:
+ * domingo -> 1, lunes -> 2, martes -> 3, etc...
+ */
+ let dayWeek = prompt("¿Qué día de la semana es hoy?")
+ dayWeek = dayWeek.toLowerCase()
+ switch (dayWeek) {
+     case "lunes":
+         console.log(`${dayWeek} es el dia 1`)
+         break
+     case "martes":
+         console.log(`${dayWeek} es el dia 2`)
+         break
+     case "miercoles":
+     case "miércoles":
+         console.log(`${dayWeek} es el dia 3`)
+         break
+     case "jueves":
+         console.log(`${dayWeek} es el dia 4`)
+         break
+     case "viernes":
+         console.log(`${dayWeek} es el dia 5`)
+         break
+     case "sabado":
+     case "sábado":
+         console.log(`${dayWeek} es el dia 6`)
+         break
+     case "domingo":
+         console.log(`${dayWeek} es el dia 7`)
+         break
+     default:
+         console.error(`Es un día invalido`)
+         break
+ }
