f6614d013e4b19821798dc135bb296d8cce8fd02
Job <bojlk2@hotmail.com>
2021-11-29 19:17:05 -0600

    homework

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,89 +1,53 @@
-// let saludo = 'hola'
-// for(i = 0; i < saludo.length; i++){
-//     console.log(saludo[i])
-// }
+/*
+	Pedir al usuario una palabra
+	Devolver la palabra capitalizada
+	p.ej. 'galAxiA'
+	Ouput -> 'Galaxia'
+*/
+let word = prompt(`Introduzca una palabra`).toLowerCase()
+let letter = word[0].toUpperCase()
 
-//Pedir al ususario un string
-// contar las vocales y las consonantes
-//imprimir en consola
-// 2 vocales y 2 consonantes
-// let word = prompt('Introduce una palabra')
-
-
-// if(word.length > 0){
-//     let vocal = 0
-//     let consonante = 0
-//     for(i = 0; i < word.length; i++){
-//             let letter = word[i].toLowerCase()
-//                 if(letter !== ' '){
-//                     if(
-//                         letter === 'a' ||
-//                         letter === 'á' ||
-//                         letter === 'e' ||
-//                         letter === 'é' ||
-//                         letter === 'i' ||
-//                         letter === 'í' ||
-//                         letter === 'o' ||
-//                         letter === 'ó' ||
-//                         letter === 'u' ||
-//                         letter === 'ú'
-//                     ) {
-//                         vocal++
-//                     } else {
-//                         consonante++
-//                 }
-                
-//             } 
-//     }console.log(`Tu palabra "${word}", tiene ${vocal} vocales y ${consonante} consonantes`)
-// } else {
-//     console.log('Inserta una palabra')
-// }
-// pedir una palanbra con espacio al inicio y al final
-// contar las letras "a" y "e", 
-// Contar numero de espacios, sin contar los espacios
-// al inicio y al final
-
-// let spword = prompt('Introduce una palabra con espacio al inicio y al final').trim().toLowerCase()
-// let a, e, space = 0
-
-// if(spword.length > 0){
-    
-// } else {
-//     console.log('Debes introducir una palabra')
-// }
-
-// Pedir el usuario 1 numero entre 1 y 100
-// sumar todos los numeros entre el 1 y este numero
-// Mandar un alert con el  total                           
-
-// 10
-// 1+2+3+4+5+6+7+8+9+10
-// total -> ?
-let num = parseInt(prompt('Introduce un número entre 1 y 100'))
-let add = 0
-if(!isNaN(num) && num > 0 && num <=100){
-    for(i = 1; i <= num; i++){
-        add += i
-    }
-    alert(`La suma de los numeros es = ${add}`)
-} else {
-    console.error(`No es un número válido`)
+for(let i = 1; i < word.length; i++){
+    letter += word[i]
 }
 
-// pedir al usuario una palabra (permitir espacios, mayusculas)
-// Verificar que sea un palindromo
-// mandar en consola el resultado
-// -> La palabra "anita lava la tina" es un palindromo
-const pal = prompt(`Introduce una palabra u oración`).replace(/ +/g, "").toLowerCase()
-let wrev = ''
+console.log(letter)              
 
-if(pal != ''){
-    for(i = pal.length - 1; i >= 0; i--){
-        wrev += pal[i]
+/*
+	Pedir al usuario una oración (permitir espacios)
+	Capitalizar cada una de las palabras de la oración
+	p.ej. 'En una galaxia muy muy lejana'
+	Ouput -> 'En Una Galaxia Muy Muy Lejana'
+*/
+const statement = prompt('Introduce un enunciado').trim().toLowerCase()
+let wordCap = ''
+
+for(i = 0; i < statement.length; i++){
+    if(statement[i] == ' '){
+        wordCap = ' ' + statement[i + 1].toUpperCase()
+        i += 1
+    } else {
+        wordCap = wordCap + statement[i]
     }
-    pal === wrev ? 
-    console.log(`La palabra "${pal}" es un palíndromo`) : 
-    console.log(`La palabra "${pal}" no es un palíndromo`)
- } else {
-    console.error(`Debe introducir una palabra u oración`)
 }
+
+wordCap = wordCap.substring(0,1).toUpperCase() + wordCap.substring(1)
+
+console.log(wordCap)
+
+/*
+	Pedir al usuario cuantas materias tiene actualmente
+	Pedir al usuario la calificacion de cada una de esas N materias
+	Arrojar el promedio en un alert
+*/
+let mat = parseInt(prompt(`¿Cuantas materias cursas?`))
+let qual = 0
+let sumqual = 0
+
+for(k = 1; k <= mat; k++){
+    qual = parseInt(prompt(`¿Cuál es tu calificación?`))
+    sumqual += qual
+    console.log(sumqual)
+}
+
+console.log(`Tu promedio es de: ${sumqual / mat}`)
--- assets/js/homework.js
+++ assets/js/homework.js
@@ -1,16 +1,72 @@
-let pal1 = prompt(`Introduce una palabra u oración`).replace(/ +/g, "").toLowerCase()
-let wrev1 = ''
-let count = 0
+/** 
+ * Ejercicio 1
+ * imprimir en consola todos los numeros pares entre 1 y 100
+ * -> 2
+ * -> 4
+ * -> 100
+*/
 
-if(pal1 != ''){
-    while(count <= pal1.length -1 ){
-        wrev1 += count
-        count++
+let par = 0
+
+for(i = 1; i <= 100; i++){
+    if(par = i % 2){
+        i += 1
+        console.log(i)
     }
-    console.log(wrev1)
-    pal1 === wrev1 ? 
-    console.log(`La palabra "${pal1}" es un palíndromo`) : 
-    console.log(`La palabra "${pal1}" no es un palíndromo`)
-} else {
-    console.error(`Debe introducir una palabra u oración`)
+    
 }
+
+/**
+ * Ejercicio 2
+ * Pedir al usuario un string
+ * Invertir el orden e imprimir en consola
+ * Entrada: "reverse"
+ * Salida: -> esrever
+ * hint: https://www.w3schools.com/jsref/jsref_length_string.asp
+*/
+
+let word = prompt('Introduzca una palabra')
+let wr = ''
+
+for(j = word.length -1; j >= 0; j--){
+    wr += word[j]
+}
+console.log(wr)
+
+/**
+ * Ejercicio 3 
+ * Declarar un string de al menos 2 palabras
+ * Imprimir en consola el string separado por guiones en cada palabra
+ * "hola koders"
+ * "hola-koders"
+ * hint: https://www.w3schools.com/jsref/jsref_replace.asp
+ */
+
+let phrase = prompt('Introduce mas de 2 palabras').replace(/ /gi,'-')
+console.log(phrase)
+
+/**
+ * Ejercicio 4
+ * Declarar un string con un correo electrónico
+ * Enmascarar con "*" el bloque del usuario
+ * "jorgec@kodemia.mx"
+ * "*****@kodemia.mx"
+ * 
+ * hint: https://www.w3schools.com/jsref/jsref_slice_string.asp
+ * hint: https://www.w3schools.com/jsref/jsref_indexof.asp
+ */
+
+let mail = prompt('Introdroduce tu correo electronico')
+let ocult = ''
+let pri = 0
+let reg = /\d.*\d/
+for(k = mail.length -1; k >= 0; k--){
+    ocult += mail[k]
+}
+pri = ocult.indexOf('@')
+ocult = ocult.slice(pri + 1)
+ocult = ocult.replace(/[a-z,0-9]/gi, '*')
+for
+console.log(pri)
+console.log(ocult)
+
