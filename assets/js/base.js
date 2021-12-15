21bebe0a0e42db53c0c7c1fbbc289d2f8e0065be
Job <bojlk2@hotmail.com>
2021-12-14 19:26:36 -0600

    fiftÂeen

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,59 +1,3 @@
-/**
- * Ejercicio 1.
- * Dado el siguiente Array ['Hoteles', 'Ofertas', 'Viajes', 'Ayuda', 'Cancelaciones']
- * Formar el siguiente markup en el DOM
- * <ul class="menu__booking" data-menu="booking">
-        <li class="menu__booking__item">Hoteles</li>
-        <li class="menu__booking__item">Ofertas</li>
-        <li class="menu__booking__item">Viajes</li>
-        <li class="menu__booking__item">Ayuda</li>
-        <li class="menu__booking__item">Cancelaciones</li>
-    </ul>
- * 
- */
-
-    let menuContainer = document.createElement('ul')
-    menuContainer.classList
-
-const reverseUser = ( ) => {
-    let word =document.querySelector('#username').value
-    let revw = word.split('').reverse().join('')
-    console.log(revw)
-  }
-
-  /**
- * Dado un array
- * ['colima', 'colombia','Estado de mexico']
- * 
- * EScuchar el evento keyup en un input
- * Filtrar del array las coincidencias
- * Pintar las coincidencias en una lista
- * 
- */
-
-let cities = ['colima', 'colombia','Estado de mexico']
-
-const filterCity = () => {
-    // filtrar ciudades
-    let citySearch = document.querySelector('#name__city').value
-    let citiesFiltered = cities.filter( (city) => {
-        if(city.includes(citySearch)){
-            return city
-        }
-    })
-
-    // creo el layout con las ciudades filtradas
-    let lista = ''
-    citiesFiltered.forEach( (value) => {
-        lista += `<li>${value}</li>`
-    })
-
-    // agrego el layout
-    document.querySelector('#listCity').innerHTML = lista
-}
-
-
-
 let koders = [
     {
         name: 'emilio',
@@ -61,118 +5,46 @@ let koders = [
         city: 'Guadalajara'
     },
     {
-        name: 'Brisset',
-        age: 30,
-        city: 'Lima'
+        name: 'juan',
+        age: 32,
+        city: 'Zacatecas'
     },
     {
-        name: 'Job',
-        age: 33,
-        city: 'CDMX'
-    }
-]
-
-const filterKoder = () => {
-    let koderSearch = document.querySelector('#name__koder').value.toLowerCase()
-    let kodersFiltered = koders.filter((koder) => {
-        if(koder.name.toLowerCase().includes(koderSearch)){
-            return koder
-        }
-    })
-
-    let koderList = ''
-    kodersFiltered.forEach( (value) => {
-        koderList += `<li><strong>${value.name}</strong>
-        <span>${value.age}</span>
-        <span>${value.city}</span></li>`
-    })
-
-    document.querySelector('#listKoders').innerHTML = koderList
-}
-
-const filterKoderAge = () => {
-    let koderSearchAge = document.querySelector('#age__koder').value.toLowerCase()
-    let kodersFilteredAge = koders.filter( (koderAge) => {
-        if(koderAge.age.toLowerCase().includes(koderSearchAge)){
-            return koderAge
-        }
-    })
-
-    let koderListAge = ''
-    kodersFilteredAge.forEach((value) => {
-        koderListAge += `<li><strong>${value.name}</strong>
-        <span>${value.age}</span>
-        <span>${value.city}</span></li>`
-    })
-
-    document.querySelector('#listKodersAge').innerHTML = koderListAge
-}
-
-const filterKoderCity = () => {
-    let koderSearchCity = document.querySelector('#city__koder').value.toLowerCase()
-    let kodersFilteredCity = koders.filter( (koderCity) => {
-        if(koderCity.city.toLowerCase().includes(koderSearchCity)){
-            return koderCity
-        }
-    })
-
-    let koderListCity = ''
-    kodersFilteredCity.forEach((value) => {
-        koderListCity += `<li><strong>${value.name}</strong>
-        <span>${value.age}</span>
-        <span>${value.city}</span></li>`
-    })
-
-    document.querySelector('#listKodersCity').innerHTML = koderListCity
-}
-
-
-
-const changeFilter = () => {
-    console.log('Cambio de filtro')
-    let filter = document.querySelector('#filterby').value
-    
-    switch (filter) {
-        case 'name':
-            document.getElementById("name__koder").style.display = "inline-block"
-            document.getElementById("age__koder").style.display = "none"
-            document.getElementById("city__koder").style.display = "none"
-            break
-
-        case "age":
-            document.getElementById("age__koder").style.display = "inline-block"
-            document.getElementById("name__koder").style.display = "none"
-            document.getElementById("city__koder").style.display = "none"
-            break
-
-        case "city":
-            document.getElementById("city__koder").style.display = "inline-block"
-            document.getElementById("name__koder").style.display = "none"
-            document.getElementById("age__koder").style.display = "none"
-            break
-    }
-}
-
-let koders = [
-    {
-        name: 'emilio',
-        age: 30,
-        city: 'Guadalajara'
-    },
-    {
-        name: 'emilio',
-        age: 30,
-        city: 'guanajuato'
-    },
-    {
-        name: 'Brisset',
+        name: 'brisset',
         age: 37,
         city: 'Lima'
-    }
+    },
+    
 ]
+
+
+const orderDesc = (a, b) => {
+    let koderascdesc = document.querySelector('#order').value
+    let KoderTerm = document.querySelector('#filterby').value.toLowerCase()
+    
+    if(koderascdesc === 'desc') { 
+        if (a[KoderTerm] < b[KoderTerm]) {
+            return -1
+        }
+        if (a[KoderTerm] > b[KoderTerm]) {
+            return 1
+        }
+        return 0
+    } else {
+        if (a[KoderTerm] > b[KoderTerm]) {
+            return -1
+        }
+        if (a[KoderTerm] < b[KoderTerm]) {
+            return 1
+        }
+        return 0
+    }
+    console.log(koders.sort(orderDesc))
+}
 
 
 // FUncion => tarea especifica
+
 
 const filterKoders = () => {
     // filtrar ciudades
@@ -201,9 +73,9 @@ const filterKoders = () => {
             }
         }
     })
+    console.log(kodersFiltered.sort(orderDesc))
 
-    console.log(kodersFiltered)
-
+    
     // creo el layout con las ciudades filtradas
     let lista = ''
     kodersFiltered.forEach( (koder) => {
@@ -215,6 +87,7 @@ const filterKoders = () => {
             </li>
         `
     })
+    
     console.log(lista)
 
     // agrego el layout
@@ -230,3 +103,4 @@ const filterKoder = () => {
 const changeFilter = () => {
     filterKoders()
 }
+
--- assets/js/homework.js
+++ assets/js/homework.js
@@ -1,31 +1,142 @@
-let ulMenu = document.createElement('ul')
-let liHotels = document.createElement('li')
-let liOffers = document.createElement('li')
-let liTravels = document.createElement('li')
-let liHelp = document.createElement('li')
-let liCancel = document.createElement('li')
+let koders = [
+    {
+        name: 'Emilio',
+        age: 30,
+        city: 'Guadalajara'
+    },
+    {
+        name: 'Pedro',
+        age: 30,
+        city: 'Leon'
+    },
+    {
+        name: 'Brisset',
+        age: 37,
+        city: 'Lima'
+    },
+    {
+        name: 'alex',
+        age: 37,
+        city: 'Lima'
+    },
+    {
+        name: 'Brisset',
+        age: 37,
+        city: 'Lima'
+    }
+]
 
-ulMenu.setAttribute('class', 'menu__booking')
-ulMenu.setAttribute('data-menu', 'booking')
+document.addEventListener('onchange', orderArray)
 
-liHotels.setAttribute('class', 'menu__booking__item')
-liHotels.textContent = 'Hoteles'
-ulMenu.appendChild(liHotels)
+const orderArray = (arr) => {
+    let copyArray = [...arr]
+    
+    let order = document.querySelector('#orderFilter').value
+    
+    let KoderTerm = document.querySelector('#filterby').value
+    if(KoderTerm !== 'age'){
+        if(order == 'asc') {
+            copyArray.sort(function (firstEl, secondEl) {
+                let a = firstEl[KoderTerm].toUpperCase(),
+                b = secondEl[KoderTerm].toUpperCase();
+                //return a == b ? 0 : a > b ? 1 : -1
+                if(a > b) {
+                    return 1
+                } 
+                if (a < b) {
+                    return -1
+                }
+                if(a == b){
+                    return 0
+                }
+            })
+        } else {
+            console.log(KoderTerm)
+            copyArray.sort(function (firstEl, secondEl) {
+                let a = firstEl[KoderTerm].toUpperCase(),
+                b = secondEl[KoderTerm].toUpperCase();
+                // return b == a ? 0 : b > a ? 1 : -1;
+                if(b > a) {
+                    return 1
+                } 
+                if (b < a) {
+                    return -1
+                }
+                if(b == a){
+                    return 0
+                }
+            })
+        }
+    } else {
+        if(order == 'asc'){
+            copyArray.sort( function ( a, b ) { 
+                return a[KoderTerm] - b[KoderTerm]
+            }) 
+        } else {
+            copyArray.sort( function ( a, b ) {
+                console.log(a,b) 
+                return b[KoderTerm] - a[KoderTerm]
+            })
+        }
+    }
 
-liOffers.setAttribute('class', 'menu__booking__item')
-liOffers.textContent = 'Ofertas'
-ulMenu.appendChild(liOffers)
+    return copyArray
+    
+}
 
-liTravels.setAttribute('class', 'menu__booking__item')
-liTravels.textContent = 'Viajes'
-ulMenu.appendChild(liTravels)
+// FUncion => tarea especifica
+const filterKoders = () => {
+    // filtrar ciudades
+    let KoderSearch = document.querySelector('#name__koder').value.toLowerCase()
+    let KoderTerm = document.querySelector('#filterby').value.toLowerCase()
 
-liHelp.setAttribute('class', 'menu__booking__item')
-liHelp.textContent = 'Ayuda'
-ulMenu.appendChild(liHelp)
+    let kodersFiltered = koders.filter( (koder) => {        
+        let koderTerm = typeof koder[KoderTerm] !== 'number' ? koder[KoderTerm].toLowerCase() : koder[KoderTerm]
 
-liCancel.setAttribute('class', 'menu__booking__item')
-liCancel.textContent = 'Cancelaciones'
-ulMenu.appendChild(liCancel)
+        if(KoderTerm === 'age') {
+            if((koderTerm).toString().includes(KoderSearch) === true){
+                return koder
+            }
+        } else  {
+            if(koderTerm.includes(KoderSearch) === true){
+                return koder
+            }
+        }
+    })
 
-document.getElementsByTagName('body')[0].appendChild(ulMenu)
+    // console.log(kodersFiltered)
+    console.log('koders filtrados')
+    console.log(kodersFiltered)
+
+    let kodersFilteredOrdered = orderArray(kodersFiltered)
+    console.log('koders filtrados y ordenados')
+    console.log(kodersFilteredOrdered)
+
+    // creo el layout con las ciudades filtradas
+    let lista = ''
+    kodersFilteredOrdered.forEach( (koder) => {
+        lista += `
+            <li>
+                <strong>${koder.name}</strong>
+                <span>${koder.age} años</span>
+                <span>${koder.city}</span>
+            </li>
+        `
+    })
+
+    // agrego el layout
+    document.querySelector('#listKoders').innerHTML = lista
+}
+
+
+const orderFilter = () => {
+    filterKoders()
+}
+
+const filterKoder = () => {
+    filterKoders()
+}
+
+const changeFilter = () => {
+    filterKoders()
+}
